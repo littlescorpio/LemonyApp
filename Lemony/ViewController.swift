fc0c56af3472ccbf2919110a746f02a1fb903714
Jason Lien <udonz@192-168-1-108.tpgi.com.au>
2015-03-18 20:40:54 +1100

    Initial Commit

---
+++ Lemony/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lemony
+//
+//  Created by Jason Lien on 18/03/2015.
+//  Copyright (c) 2015 littlescorpio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
